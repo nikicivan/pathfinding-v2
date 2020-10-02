f4b6f4aa741de41e071d0719ae0e244c9cf72aaf
unknown <ivan.nikic@gmail.com>
2020-10-02 22:11:06 +0200

    Updates

---
+++ precache-manifest.b62a48eafd3ce6e291f215146c1e498c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a683b2dc5ef48b2c0adab86477deac",
+    "url": "/pathfinding-v2/index.html"
+  },
+  {
+    "revision": "c49cd8c34bd289f9a381",
+    "url": "/pathfinding-v2/static/js/2.1c67933b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pathfinding-v2/static/js/2.1c67933b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bad0b37f4ab1bf441c",
+    "url": "/pathfinding-v2/static/js/main.9b6dec1a.chunk.js"
+  },
+  {
+    "revision": "5d75ab160f1f3795ac57",
+    "url": "/pathfinding-v2/static/js/runtime-main.36a6c942.js"
+  }
+]);
